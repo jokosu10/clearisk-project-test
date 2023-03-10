ab48061f9697f26e03ea9ceafa8ba5ab7542f7f0
Joko Susilo <susilo.j8@gmail.com>
2023-03-10 11:32:38 +0700

    add api sum balance payment by status payment (#13)

--- models/payments.go
+++ models/payments.go
@@ -10,3 +10,14 @@ type Sumbalanceperiode_response_single struct {
 	Message string                       `json:"message"`
 	Data    SumOfBalancePaymentsByPeriod `json:"data"`
 }
+
+type SumOfBalancePaymentsByStatus struct {
+	Status       string
+	STATUS_COUNT string
+}
+type Sumbalancestatus_response_single struct {
+	Code    int                            `json:"code"`
+	Status  string                         `json:"status"`
+	Message string                         `json:"message"`
+	Data    []SumOfBalancePaymentsByStatus `json:"data"`
+}
